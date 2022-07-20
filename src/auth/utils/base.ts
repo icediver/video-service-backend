7d1d0d180905765ec63a179ef5edeb319b191019
Artem Ageev <icediver@MacBook-Pro-Artem.local>
2022-07-21 01:19:23 +0300

    Add users, video, auth, comments

---
+++ src/auth/utils/base.ts
@@ -0,0 +1,15 @@
+import {
+	CreateDateColumn,
+	PrimaryGeneratedColumn,
+	UpdateDateColumn,
+} from 'typeorm';
+
+export abstract class Base {
+	@PrimaryGeneratedColumn()
+	id: number;
+
+	@CreateDateColumn({ name: 'created_at' })
+	createdAt: Date;
+	@UpdateDateColumn({ name: 'update_at' })
+	updateAt: Date;
+}
